e3f8e855c044649e4ac4969a61f39dbbe6084d1c
SUSHMIT SRIVASTAVA <83881402+Sushmit0706@users.noreply.github.com>
2023-04-21 00:24:46 +0530

    Update index.js

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import { useRef } from 'react'
+
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 // import avatar from '../../../assets/images/avatar.jpg'
 import LogoTitle from '../../../assets/images/programmer.png'
 
@@ -13,32 +13,6 @@ const Logo = () => {
     const outlineLogoRef = useRef()
     const solidLogoRef = useRef()
 
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
-
-        gsap
-            .timeline()
-            .to(bgRef.current, {
-                duration: 1,
-                opacity: 1,
-            })
-            .from(outlineLogoRef.current, {
-                drawSVG: 0,
-                duration: 20,
-            })
-
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    }, [])
     return (
         <div className="logo-container" ref={bgRef}>
             <img ref={solidLogoRef} className="solid-logo" src={LogoTitle} alt="S" />
